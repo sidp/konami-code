54ab860a8c2709072398f1bbd73316c81c726daa
Peter Simonsson <peter@wenderfalck.com>
2015-06-03 19:26:44 +0200

    Initial code commit.

---
+++ konami-code.js
@@ -0,0 +1,46 @@
+var KonamiCode = (function () {
+  "use strict";
+
+  function KonamiCode(fn) {
+    if (!(this instanceof KonamiCode)) {
+      throw new Error("Constructor called as a function");
+    }
+
+    this.keys = [];
+    this.code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    this.fn = fn || function () {};
+
+    document.addEventListener('keydown', function (ev) {
+      this.capture(ev.keyCode);
+      this.validate();
+    }.bind(this));
+  }
+
+  KonamiCode.prototype.capture = function (code) {
+    this.keys.push(code);
+    if (this.keys.length > this.code.length) {
+      this.keys.splice(0, 1);
+    }
+  }
+
+  KonamiCode.prototype.validate = function () {
+    var correct = true;
+    if (this.keys.length === this.code.length) {
+      for (var i = 0; i < this.keys.length; i++) {
+        if (this.keys[i] != this.code[i]) {
+          correct = false;
+          break;
+        }
+      }
+      if (correct) {
+        this.fire();
+      }
+    }
+  }
+
+  KonamiCode.prototype.fire = function () {
+    this.fn();
+  }
+
+  return KonamiCode;
+})();
