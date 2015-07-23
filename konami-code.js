8229a5c6ceab9e858fede125e5d729f11733ace3
Peter Simonsson <sidp86@gmail.com>
2015-07-23 11:20:21 +0200

    Adding support for AMD and CommonJS.

--- konami-code.js
+++ konami-code.js
@@ -1,4 +1,12 @@
-var KonamiCode = (function () {
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = factory();
+  } else {
+    root.KonamiCode = factory();
+  }
+}(this, function () {
   "use strict";
 
   function KonamiCode(fn) {
@@ -43,4 +51,4 @@ var KonamiCode = (function () {
   };
 
   return KonamiCode;
-})();
+}));
